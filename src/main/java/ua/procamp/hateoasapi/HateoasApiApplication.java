8d1a2726698d2e86323b9cdc7e619d56830fff33
shtramak <shtramak@gmail.com>
2019-04-29 17:02:21 +0300

    initial commit

---
+++ src/main/java/ua/procamp/hateoasapi/HateoasApiApplication.java
@@ -0,0 +1,13 @@
+package ua.procamp.hateoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/procamp/hateoasapi/HateoasApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.procamp.hateoasapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HateoasApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
